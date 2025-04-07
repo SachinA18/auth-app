7571aff67f0c581c2a80eba30ad229b40da05ae8
SachinA18 <sachinthaariyawansha98@gmail.com>
2025-04-07 20:18:48 +0530

    Bug fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b42cb58fc510030"; }
+/******/ 		__webpack_require__.h = function() { return "dae8d56a973325ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
